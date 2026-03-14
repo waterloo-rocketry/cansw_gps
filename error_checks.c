a40c3e57b6cf83fcdc287940611c39281c7939f8
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2026-03-14 18:04:04 -0400

    Update canlib to 2026.3

--- error_checks.c
+++ error_checks.c
@@ -11,16 +11,16 @@
 //******************************************************************************
 
 bool check_5v_current_error(void) {
-    adc_result_t sense_raw_mV = ADCC_GetSingleConversion(channel_CURRENT) / 2;
-    int curr_draw_mA = (sense_raw_mV) / 20;
+	adc_result_t sense_raw_mV = ADCC_GetSingleConversion(channel_CURRENT) / 2;
+	int curr_draw_mA = (sense_raw_mV) / 20;
 
-    can_msg_t msg;
-    build_analog_data_msg(PRIO_LOW, millis(), SENSOR_5V_CURR, curr_draw_mA, &msg);
-    txb_enqueue(&msg);
+	can_msg_t msg;
+	build_analog_sensor_16bit_msg(PRIO_LOW, millis(), SENSOR_5V_CURR, curr_draw_mA, &msg);
+	txb_enqueue(&msg);
 
-    if (curr_draw_mA > OVERCURRENT_THRESHOLD_mA) {
-        return true;
-    }
+	if (curr_draw_mA > OVERCURRENT_THRESHOLD_mA) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
