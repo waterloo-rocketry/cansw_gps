7d1e73f8043368707f726e65fa044bff41cd4d1c
Skyler Ma <skyler@skylerma.com>
2026-07-29 23:48:42 -0400

    Run clang-format

--- gps_general.c
+++ gps_general.c
@@ -3,49 +3,49 @@
 #include "gps_general.h"
 
 void uart_init(void) {
-    // Set Baud Rate Generator to generate baud rate of 9600
-    // Bit 7 (BRGS) set to 1 to enable high speed BRG
-    // Configure mode pins <3:0> 0000 sets the mode to 8 bit no parity
-    U1CON0bits.BRGS = 1;
-    U1CON0bits.MODE = 0;
+	// Set Baud Rate Generator to generate baud rate of 9600
+	// Bit 7 (BRGS) set to 1 to enable high speed BRG
+	// Configure mode pins <3:0> 0000 sets the mode to 8 bit no parity
+	U1CON0bits.BRGS = 1;
+	U1CON0bits.MODE = 0;
 
-    // _XTAL_FREQ / 9600 / 16 - 1
-    U1BRGH = 0x4;
-    U1BRGL = 0xE1;
+	// _XTAL_FREQ / 9600 / 16 - 1
+	U1BRGH = 0x4;
+	U1BRGL = 0xE1;
 
-    // Set RX1 to PORT C3
-    U1RXPPS = 0b010011;
+	// Set RX1 to PORT C3
+	U1RXPPS = 0b010011;
 
-    // Set the ON bit
-    //  Bit7=ON, Bit3 = RXBIMD(Receive Break Interrupt Mode Select bit)
-    U1CON1 = 0b10001000;
-    //  Run on overflow
-    U1CON2bits.RUNOVF = 1;
-    // Set U1TXIE to enable interrupt
-    PIE3bits.U1RXIE = 1;
-    // Enable reception by setting RXEN
-    U1CON0bits.RXEN = 1;
-    // Configure RX pin at C3
-    LATC3 = 1;
-    ANSELC3 = 0;
-    TRISC3 = 1;
+	// Set the ON bit
+	//  Bit7=ON, Bit3 = RXBIMD(Receive Break Interrupt Mode Select bit)
+	U1CON1 = 0b10001000;
+	//  Run on overflow
+	U1CON2bits.RUNOVF = 1;
+	// Set U1TXIE to enable interrupt
+	PIE3bits.U1RXIE = 1;
+	// Enable reception by setting RXEN
+	U1CON0bits.RXEN = 1;
+	// Configure RX pin at C3
+	LATC3 = 1;
+	ANSELC3 = 0;
+	TRISC3 = 1;
 
-    U1ERRIRbits.U1FERIF = 0;
-    // End of UART connection setup
+	U1ERRIRbits.U1FERIF = 0;
+	// End of UART connection setup
 }
 
 void led_init(void) {
-    TRISC0 = 0;
-    LED_1_OFF();
+	TRISC0 = 0;
+	LED_1_OFF();
 
-    TRISC1 = 0;
-    LED_2_OFF();
+	TRISC1 = 0;
+	LED_2_OFF();
 }
 
 void led_1_heartbeat(void) {
-    if (LATC0) {
-        LED_1_OFF();
-    } else {
-        LED_1_ON();
-    }
+	if (LATC0) {
+		LED_1_OFF();
+	} else {
+		LED_1_ON();
+	}
 }
--- main.c
+++ main.c
@@ -23,164 +23,164 @@ static volatile bool recieved_first_message = false;
 static volatile bool seen_can_message = false;
 
 int main(void) {
-    // Set frequency to be 48 MHZ
-    OSCFRQbits.FRQ = 0b0111;
+	// Set frequency to be 48 MHZ
+	OSCFRQbits.FRQ = 0b0111;
 
-    ADCC_Initialize();
-    FVR_Initialize();
+	ADCC_Initialize();
+	FVR_Initialize();
 
-    // Enable global interrupts
-    INTCON0bits.GIE = 1;
+	// Enable global interrupts
+	INTCON0bits.GIE = 1;
 
-    uart_init();
-    led_init();
-    gps_init();
-    timer0_init();
+	uart_init();
+	led_init();
+	gps_init();
+	timer0_init();
 
-    // Set up CAN TX
-    TRISB5 = 0;
-    RB5PPS = 0x33;
+	// Set up CAN TX
+	TRISB5 = 0;
+	RB5PPS = 0x33;
 
-    // Set up CAN RX
-    TRISB4 = 1;
-    ANSELB4 = 0;
-    CANRXPPS = 0x0C;
+	// Set up CAN RX
+	TRISB4 = 1;
+	ANSELB4 = 0;
+	CANRXPPS = 0x0C;
 
-    // set up CAN module
-    can_timing_t can_setup;
-    can_generate_timing_params(_XTAL_FREQ, &can_setup);
-    pic18f26k83_can_init(&can_setup, can_msg_handler);
-    txb_init(tx_pool, sizeof(tx_pool), pic18f26k83_can_send, pic18f26k83_can_send_rdy);
+	// set up CAN module
+	can_timing_t can_setup;
+	can_generate_timing_params(_XTAL_FREQ, &can_setup);
+	pic18f26k83_can_init(&can_setup, can_msg_handler);
+	txb_init(tx_pool, sizeof(tx_pool), pic18f26k83_can_send, pic18f26k83_can_send_rdy);
 
-    uint32_t last_millis = millis();
-    uint32_t last_message_millis = millis();
+	uint32_t last_millis = millis();
+	uint32_t last_message_millis = millis();
 
-    // Turn LED 1 on
-    LED_1_ON();
+	// Turn LED 1 on
+	LED_1_ON();
 
-    // Wait for the first message
-    while (!recieved_first_message) {
-        CLRWDT(); // feed the watchdog, which is set for 256ms
+	// Wait for the first message
+	while (!recieved_first_message) {
+		CLRWDT(); // feed the watchdog, which is set for 256ms
 
-        if (seen_can_message) {
-            seen_can_message = false;
-            last_message_millis = millis();
-        }
+		if (seen_can_message) {
+			seen_can_message = false;
+			last_message_millis = millis();
+		}
 
-        if (millis() - last_message_millis > MAX_BUS_DEAD_TIME_ms) {
-            // We've got too long without seeing a valid CAN message (including one of ours)
-            RESET();
-        }
+		if (millis() - last_message_millis > MAX_BUS_DEAD_TIME_ms) {
+			// We've got too long without seeing a valid CAN message (including one of ours)
+			RESET();
+		}
 
-        if (millis() - last_millis > 5000) {
-            // Haven't received anything, try resetting the gps
-            LATC5 = 0;
-            __delay_ms(100);
-            LATC5 = 1;
-            __delay_ms(100);
+		if (millis() - last_millis > 5000) {
+			// Haven't received anything, try resetting the gps
+			LATC5 = 0;
+			__delay_ms(100);
+			LATC5 = 1;
+			__delay_ms(100);
 
-            can_msg_t board_stat_msg;
-            build_general_board_status_msg(PRIO_LOW, millis(), (1 << E_IO_ERROR_OFFSET), &board_stat_msg);
-            txb_enqueue(&board_stat_msg);
+			can_msg_t board_stat_msg;
+			build_general_board_status_msg(
+				PRIO_LOW, millis(), (1 << E_IO_ERROR_OFFSET), &board_stat_msg);
+			txb_enqueue(&board_stat_msg);
 
-            last_millis = millis();
-        }
-    }
+			last_millis = millis();
+		}
+	}
 
-    while (1) {
-        CLRWDT(); // feed the watchdog, which is set for 256ms
+	while (1) {
+		CLRWDT(); // feed the watchdog, which is set for 256ms
 
-        if (seen_can_message) {
-            seen_can_message = false;
-            last_message_millis = millis();
-        }
+		if (seen_can_message) {
+			seen_can_message = false;
+			last_message_millis = millis();
+		}
 
-        if (millis() - last_message_millis > MAX_BUS_DEAD_TIME_ms) {
-            // We've got too long without seeing a valid CAN message (including one of ours)
-            RESET();
-        }
+		if (millis() - last_message_millis > MAX_BUS_DEAD_TIME_ms) {
+			// We've got too long without seeing a valid CAN message (including one of ours)
+			RESET();
+		}
 
-        if (millis() - last_millis > MAX_LOOP_TIME_DIFF_ms) {
-            uint32_t general_error_bitfield = 0;
-            if (check_5v_current_error()) {
-                general_error_bitfield |= (1 << E_5V_OVER_CURR_OFFSET);
-            }
+		if (millis() - last_millis > MAX_LOOP_TIME_DIFF_ms) {
+			uint32_t general_error_bitfield = 0;
+			if (check_5v_current_error()) {
+				general_error_bitfield |= (1 << E_5V_OVER_CURR_OFFSET);
+			}
 
-            can_msg_t board_stat_msg;
-            build_general_board_status_msg(
-                PRIO_LOW, millis(), general_error_bitfield, &board_stat_msg
-            );
-            txb_enqueue(&board_stat_msg);
+			can_msg_t board_stat_msg;
+			build_general_board_status_msg(
+				PRIO_LOW, millis(), general_error_bitfield, &board_stat_msg);
+			txb_enqueue(&board_stat_msg);
 
-            led_1_heartbeat();
-            last_millis = millis();
-        }
-        for (int i = 0; i < 1000; i++) {} // FIXME workaround to prevent sending message to fast
-        txb_heartbeat();
-    }
+			led_1_heartbeat();
+			last_millis = millis();
+		}
+		for (int i = 0; i < 1000; i++) {} // FIXME workaround to prevent sending message to fast
+		txb_heartbeat();
+	}
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
 static void __interrupt() interrupt_handler() {
-    if (PIR5) {
-        // Handle CAN
-        pic18f26k83_can_handle_interrupt();
-    }
+	if (PIR5) {
+		// Handle CAN
+		pic18f26k83_can_handle_interrupt();
+	}
 
-    // UART message
-    if (PIR3bits.U1RXIF == 1) {
-        recieved_first_message = true;
+	// UART message
+	if (PIR3bits.U1RXIF == 1) {
+		recieved_first_message = true;
 
-        if (U1ERRIRbits.FERIF) {
-            // UART frame error
-        }
+		if (U1ERRIRbits.FERIF) {
+			// UART frame error
+		}
 
-        if (U1ERRIRbits.RXFOIF) {
-            // UART overflowed
-            U1ERRIRbits.RXFOIF = 0;
-        }
+		if (U1ERRIRbits.RXFOIF) {
+			// UART overflowed
+			U1ERRIRbits.RXFOIF = 0;
+		}
 
-        gps_handle_byte(U1RXB);
+		gps_handle_byte(U1RXB);
 
-        PIR3bits.U1RXIF = 0;
-    }
+		PIR3bits.U1RXIF = 0;
+	}
 
-    // Timer0 has overflowed - update millis() function
-    // This happens approximately every 500us
-    if (PIE3bits.TMR0IE == 1 && PIR3bits.TMR0IF == 1) {
-        timer0_handle_interrupt();
-        PIR3bits.TMR0IF = 0;
-    }
+	// Timer0 has overflowed - update millis() function
+	// This happens approximately every 500us
+	if (PIE3bits.TMR0IE == 1 && PIR3bits.TMR0IF == 1) {
+		timer0_handle_interrupt();
+		PIR3bits.TMR0IF = 0;
+	}
 }
 
 // This is called from within can_handle_interrupt()
 static void can_msg_handler(const can_msg_t *msg) {
-    seen_can_message = true;
+	seen_can_message = true;
 
-    uint16_t msg_type = get_message_type(msg);
-    int dest_id = -1;
+	uint16_t msg_type = get_message_type(msg);
+	int dest_id = -1;
 
-    switch (msg_type) {
-        case MSG_LEDS_ON:
-            LED_1_ON();
-            LED_2_ON();
-            break;
+	switch (msg_type) {
+		case MSG_LEDS_ON:
+			LED_1_ON();
+			LED_2_ON();
+			break;
 
-        case MSG_LEDS_OFF:
-            LED_1_OFF();
-            LED_2_OFF();
-            break;
+		case MSG_LEDS_OFF:
+			LED_1_OFF();
+			LED_2_OFF();
+			break;
 
-        case MSG_RESET_CMD:
-            bool need_reset = false;
-            if ((W_SUCCESS == check_board_need_reset(msg, &need_reset)) && (need_reset)) {
-                RESET();
-            }
-            break;
+		case MSG_RESET_CMD:
+			bool need_reset = false;
+			if ((W_SUCCESS == check_board_need_reset(msg, &need_reset)) && (need_reset)) {
+				RESET();
+			}
+			break;
 
-        default:
-            // all the other ones - do nothing
-            break;
-    }
+		default:
+			// all the other ones - do nothing
+			break;
+	}
 }
