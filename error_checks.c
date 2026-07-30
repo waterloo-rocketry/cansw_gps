88907484eca61b53cfa2697f19dafa7ef7bdd7c6
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2026-07-29 20:27:59 -0400

    Fix clang format

--- error_checks.c
+++ error_checks.c
@@ -13,16 +13,16 @@
 //******************************************************************************
 
 bool check_5v_current_error(void) {
-    adc_result_t sense_raw_mV = ADCC_GetSingleConversion(channel_CURRENT) / 2;
-    int16_t curr_draw_mA = (sense_raw_mV) / 50;
+	adc_result_t sense_raw_mV = ADCC_GetSingleConversion(channel_CURRENT) / 2;
+	int16_t curr_draw_mA = (sense_raw_mV) / 50;
 
-    can_msg_t msg;
-    build_analog_sensor_16bit_msg(PRIO_LOW, millis(), SENSOR_5V_CURR, curr_draw_mA, &msg);
-    txb_enqueue(&msg);
+	can_msg_t msg;
+	build_analog_sensor_16bit_msg(PRIO_LOW, millis(), SENSOR_5V_CURR, curr_draw_mA, &msg);
+	txb_enqueue(&msg);
 
-    if (curr_draw_mA > OVERCURRENT_THRESHOLD_mA) {
-        return true;
-    }
+	if (curr_draw_mA > OVERCURRENT_THRESHOLD_mA) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
